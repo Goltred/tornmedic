194857c92b1536d4295143a61d843a26f83de459
Goltred <goltred@hotmail.com>
2018-12-19 23:33:24 -0500

    Added filtering controls

--- contentScript.js
+++ contentScript.js
@@ -23,83 +23,125 @@ function isOffline(memberElement)
 
 function isTraveling(memberElement)
 {
-    if (memberElement.innerText === 'Traveling')
+    if (memberElement)
     {
-        return true
+        if (memberElement.innerText === 'Traveling')
+        {
+            return true;
+        }
     }
+    
     return false;
 }
 
 function isInJail(memberElement)
 {
-    if (memberElement.innerText === 'Jail')
+    if (memberElement)
     {
-        return true
+        if (memberElement.innerText === 'Jail')
+        {
+            return true;
+        }
     }
+    
     return false;
 }
 
+class Counters {
+    constructor(){
+        this.okay = 0;
+        this.offlineHospital = 0;
+        this.traveling = 0;
+        this.hospital = 0;
+        this.jail = 0;
+    }
+
+    getTotal()
+    {
+        return this.okay + this.offlineHospital + this.traveling + this.jail;
+    }
+}
+
+function processMembers()
+{
+    //Define counters
+    let hidden = new Counters();
+    let displayed = new Counters();
+
+    //Retrieve filter controls
+    let showOkay = document.getElementById('chkOkay').checked;
+    let showTraveling = document.getElementById('chkTraveling').checked;
+    let showInJail = document.getElementById('chkInJail').checked;
+    let showOffline = document.getElementById('chkOffline').checked;
+
+    let members = document.querySelectorAll('ul.member-list > li');
+    members.forEach(function(m) {
+        //Hide if its Okay
+        let status = m.querySelector('div.acc-wrap > div.info-wrap > div.status > span.t-green');
+        if (status) {
+            hideLi(m, !showOkay);
+            !showOkay ? hidden.okay += 1 : displayed.okay += 1;
+            return;
+        }
+
+        //Hide if its travelling
+        let redStatus = m.querySelector('div.acc-wrap > div.info-wrap > div.status > span.t-red');
+        if (isTraveling(redStatus))
+        {
+            hideLi(m, !showTraveling);
+            !showTraveling ? hidden.traveling += 1 : displayed.traveling += 1;
+            return;
+        } 
+        else if (isInJail(redStatus))
+        {
+            hideLi(m, !showInJail);
+            !showInJail ? hidden.jail += 1 : displayed.jail += 1;
+            return;
+        }
+
+        //Member is in hospital
+        if (isOffline(m))
+        {
+            hideLi(m, !showOffline);
+            !showOffline ? hidden.offlineHospital += 1 : displayed.offlineHospital += 1;
+            hidden.hospital += 1;
+        } else {
+            displayed.hospital += 1;    
+        }
+    })
+
+    let totals = new Counters();
+    totals.okay = hidden.okay + displayed.okay;
+    totals.offlineHospital = hidden.offlineHospital + displayed.offlineHospital;
+    totals.traveling = hidden.traveling + displayed.traveling;
+    totals.hospital = hidden.hospital + displayed.hospital;
+    totals.jail = hidden.jail + displayed.jail;
+
+    updateElementInnerText('tchmtargets', totals.hospital - totals.offlineHospital);
+    updateElementInnerText('tchmokay', hidden.okay);
+    updateElementInnerText('tchmtraveling', hidden.traveling);
+    updateElementInnerText('tchmjail', hidden.jail);
+    updateElementInnerText('tchmoffline', hidden.offlineHospital);
+    updateElementInnerText('tchmhidden', hidden.getTotal());
+    updateElementInnerText('tchmshown', displayed.getTotal());
+    updateElementInnerText('tchmtotal', totals.getTotal());
+}
+
 //Add the HTML element to the page
-var xmlHttp = null;
+let xmlHttp = null;
 
 xmlHttp = new XMLHttpRequest();
 xmlHttp.open( "GET", chrome.extension.getURL("status.html"), false );
 xmlHttp.send( null );
 
-var statusElement = document.createElement("div");
+let statusElement = document.createElement("div");
 statusElement.innerHTML = xmlHttp.responseText;
-var factionWarInfo = document.querySelector("div.f-war-list").parentNode;
+let factionWarInfo = document.querySelector("div.f-war-list").parentNode;
 factionWarInfo.parentNode.insertBefore(statusElement, factionWarInfo);
 
 //Add the icon
-var iconDiv = document.getElementById("tchlogo");
-var logoPath = chrome.runtime.getURL('images/icon48.png');
+let iconDiv = document.getElementById("tchlogo");
+let logoPath = chrome.runtime.getURL('images/icon48.png');
 iconDiv.innerHTML = '<img src="' + logoPath + '"></img>';
 
-let members = document.querySelectorAll('ul.member-list > li');
-let totalCount = 0;
-let okayCount = 0;
-let offlineHospitalCount = 0;
-let travelingCount = 0;
-let hospitalCount = 0;
-let jailCount = 0
-members.forEach(function(m) {
-    //Hide if its Okay
-    let status = m.querySelector('div.acc-wrap > div.info-wrap > div.status > span.t-green');
-    if (status) {
-        hideLi(m);
-        okayCount += 1;
-        return;
-    }
-
-    //Hide if its travelling
-    let redStatus = m.querySelector('div.acc-wrap > div.info-wrap > div.status > span.t-red');
-    if (isTraveling(redStatus))
-    {
-        hideLi(m);
-        travelingCount += 1;
-        return;
-    } 
-    else if (isInJail(redStatus))
-    {
-        hideLi(m);
-        jailCount += 1;
-        return;
-    }
-
-    //Member is in hospital, hide if online
-    hospitalCount += 1;
-    if (isOffline(m))
-    {
-        hideLi(m);
-        offlineHospitalCount += 1;
-    }
-})
-
-totalCount = hospitalCount + okayCount + travelingCount + jailCount;
-updateElementInnerText('tchmtargets', hospitalCount - offlineHospitalCount);
-updateElementInnerText('tchmokay', okayCount);
-updateElementInnerText('tchmtraveling', travelingCount);
-updateElementInnerText('tchmjail', jailCount);
-updateElementInnerText('tchmoffline', offlineHospitalCount);
-updateElementInnerText('tchmtotal', totalCount);
+processMembers();
